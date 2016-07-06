6b8acd47c5ef72d338c9e8f53773c37e4bfcf7f4
lac15 <laszlo.erdohegyi@gmail.com>
2016-07-06 15:26:26 +0200

    Added constructor and methods to WindowsServer.java

--- Erdohegyi_Laszlo/2/SecondHomework/src/hu/schonherz/java/training/server/WindowsServer.java
+++ Erdohegyi_Laszlo/2/SecondHomework/src/hu/schonherz/java/training/server/WindowsServer.java
@@ -2,6 +2,31 @@ package hu.schonherz.java.training.server;
 
 public class WindowsServer extends Server {
 
+    private int ID;
+    private String name;
+    private String type;
+
+    public WindowsServer(int ID, String name, String type){
+        this.ID = ID;
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
     @Override
     public void restart() {
         System.out.println("Windows server is restarting...");
