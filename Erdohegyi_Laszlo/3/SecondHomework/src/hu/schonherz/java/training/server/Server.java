3fe7ba58e504139fbaf216d0c2490fa8026e7515
lac15 <laszlo.erdohegyi@gmail.com>
2016-07-11 16:05:01 +0200

    Copied second homework

---
+++ Erdohegyi_Laszlo/3/SecondHomework/src/hu/schonherz/java/training/server/Server.java
@@ -0,0 +1,26 @@
+package hu.schonherz.java.training.server;
+
+public abstract class Server {
+
+    private int cpuUsage;
+    private int memoryUsage;
+  
+    public int getCpuUsage() {
+        return cpuUsage;
+    }
+  
+    public int getMemoryUsage() {
+        return memoryUsage;
+    }
+  
+    public abstract void restart();
+
+    public abstract int getID();
+
+    public abstract String getName();
+
+    public abstract String getType();
+
+
+
+}
