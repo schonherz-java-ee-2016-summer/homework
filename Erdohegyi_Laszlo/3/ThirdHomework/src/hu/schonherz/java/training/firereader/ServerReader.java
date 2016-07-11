85c85f004948709c100ed757db5bf4c876fe9911
lac15 <laszlo.erdohegyi@gmail.com>
2016-07-11 16:10:33 +0200

    Added ServerReader and SystemAdministratorReader SOLID versions

--- Erdohegyi_Laszlo/3/ThirdHomework/src/hu/schonherz/java/training/firereader/ServerReader.java
+++ Erdohegyi_Laszlo/3/ThirdHomework/src/hu/schonherz/java/training/firereader/ServerReader.java
@@ -1,19 +1,11 @@
 package hu.schonherz.java.training.firereader;
 
-import hu.schonherz.java.training.ServerService.Database;
-import hu.schonherz.java.training.ServerService.WebContainer;
 import hu.schonherz.java.training.server.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Mindfield on 2016. 07. 03..
- */
 public class ServerReader {
 
     private static final String SUBDIRECTORY = "files";
@@ -21,30 +13,32 @@ public class ServerReader {
 
     private static File file = new File(SUBDIRECTORY + File.separator + FILENAME);
 
+    public static Server createServer(int id, String name, String type, String status){
+        if("Win".equals(type)){
+            return new WindowsDatabaseServer(id, name, type, WindowsDatabaseServer.Status.valueOf(status));
+        } else if("LinuxWeb".equals(type)){
+            return new LinuxWebServer(id, name, type, LinuxWebServer.Status.valueOf(status));
+        } else if ("LinuxDBandWEB".equals(type)){
+            return new LinuxDatabaseAndWebServer(id, name, type, LinuxDatabaseAndWebServer.Status.valueOf(status));
+        }
+        return null;
+    }
+
     public static List<Server> readFromTextFile(){
         List<Server> result = new ArrayList<>();
 
-        BufferedReader bufferedreader = null;
-
-        try {
-            bufferedreader = new BufferedReader(new FileReader(file));
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line;
 
-            while((line = bufferedreader.readLine()) != null){
+            while((line = bufferedReader.readLine()) != null){
                 String[] attributes = line.split(",");
 
-                if("Win".equals(attributes[2]) && "STOPPED".equals(attributes[3])){
-                    result.add(new WindowsDatabaseServer(Integer.parseInt(attributes[0]), attributes[1], attributes[2], Database.Status.valueOf(attributes[3])));
-                } else if("LinuxWeb".equals(attributes[2]) && "STOPPED".equals(attributes[3])){
-                    result.add(new LinuxWebServer(Integer.parseInt(attributes[0]), attributes[1], attributes[2], WebContainer.Status.valueOf(attributes[3])));
-                } else if ("LinuxDBandWEB".equals(attributes[2]) && "DATABASESTOPPED".equals(attributes[3])){
-                    result.add(new LinuxDatabaseAndWebServer(Integer.parseInt(attributes[0]), attributes[1], attributes[2], LinuxDatabaseAndWebServer.Status.valueOf(attributes[3])));
-                }
-
+                result.add(createServer(Integer.parseInt(attributes[0]), attributes[1], attributes[2], attributes[3]));
             }
-
+        }catch (FileNotFoundException e) {
+            System.out.println("File is not found.");
         }catch (IOException e){
-            System.out.println("io error");
+            System.out.println("File is corrupted.");
         }
 
         return result;
--- Erdohegyi_Laszlo/3/ThirdHomework/src/hu/schonherz/java/training/firereader/SystemAdministratorReader.java
+++ Erdohegyi_Laszlo/3/ThirdHomework/src/hu/schonherz/java/training/firereader/SystemAdministratorReader.java
@@ -3,16 +3,10 @@ package hu.schonherz.java.training.firereader;
 import hu.schonherz.java.training.pojo.SystemAdministrator;
 import hu.schonherz.java.training.server.Server;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Mindfield on 2016. 07. 03..
- */
 public class SystemAdministratorReader {
 
     private static final String SUBDIRECTORY = "files";
@@ -20,34 +14,37 @@ public class SystemAdministratorReader {
 
     private static File file = new File(SUBDIRECTORY + File.separator + FILENAME);
 
+    public static List<Server> createServerList(String [] lineAttributes){
+        List<Server> servers = new ArrayList<>();
+        List<Server> readServers = ServerReader.readFromTextFile();
+
+        for(int i = 2; i < lineAttributes.length; i++){
+            for(Server s : readServers){
+                if(Integer.parseInt(lineAttributes[i]) == s.getID()){
+                    servers.add(s);
+                }
+            }
+        }
+
+        return servers;
+    }
+
     public static List<SystemAdministrator> readFromTextFile(){
         List<SystemAdministrator> result = new ArrayList<>();
 
-        BufferedReader bufferedreader = null;
-
-        try{
-            bufferedreader = new BufferedReader(new FileReader(file));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line;
 
-            while((line = bufferedreader.readLine()) != null){
+            while((line = bufferedReader.readLine()) != null){
                 String [] attributes = line.split(",");
-                List<Server> servers = new ArrayList<>();
-                List<Server> serversreader = ServerReader.readFromTextFile();
 
-                for(int i = 2; i < attributes.length; i++){
-                    for(Server s: serversreader){
-                        if(Integer.parseInt(attributes[i]) == s.getID()){
-                            servers.add(s);
-                        }
-                    }
-                }
-
-
-                result.add(new SystemAdministrator(attributes[0], Integer.parseInt(attributes[1]), servers));
+                result.add(new SystemAdministrator(attributes[0], Integer.parseInt(attributes[1]), createServerList(attributes)));
             }
 
+        }catch (FileNotFoundException e) {
+            System.out.println("File is not found.");
         }catch (IOException e){
-            System.out.println("io error");
+            System.out.println("File is corrupted.");
         }
 
         return result;
