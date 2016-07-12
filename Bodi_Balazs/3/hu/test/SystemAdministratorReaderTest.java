3cdcd6ac07036b2e5ad9e8a619cdc7328eabdedf
bmbali <bmbali1992@gmail.com>
2016-07-12 10:50:51 +0200

    Tests added

--- Bodi_Balazs/3/hu/test/SystemAdministratorReaderTest.java
+++ Bodi_Balazs/3/hu/test/SystemAdministratorReaderTest.java
@@ -2,14 +2,11 @@ package hu.test;
 
 import hu.schonherz.java.training.firereader.SystemAdministratorReader;
 import hu.schonherz.java.training.pojo.SystemAdministrator;
-import hu.schonherz.java.training.server.LinuxDatabaseAndWebServer;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by bmbal on 2016. 07. 12..
@@ -20,22 +17,27 @@ public class SystemAdministratorReaderTest {
 
     @Test
     public void testTheNumbersOfAdministrators() throws Exception {
+        Assert.assertNotNull(actual);
         Assert.assertEquals(actual.size(), 3);
     }
     @Test
     public void testTheClassOfTheFirstAdministrator() throws Exception{
+        Assert.assertNotNull(actual);
         Assert.assertEquals(actual.get(0).getClass(), SystemAdministrator.class);
     }
     @Test
     public void testTheClassOfTheSecondAdministrator() throws Exception{
+        Assert.assertNotNull(actual);
         Assert.assertEquals(actual.get(1).getClass(), SystemAdministrator.class);
     }
     @Test
     public void testTheClassOfTheThirdAdministrator() throws Exception{
+        Assert.assertNotNull(actual);
         Assert.assertEquals(actual.get(2).getClass(), SystemAdministrator.class);
     }
     @Test
     public void testThatTheItemsAreEqual() throws Exception{
+        Assert.assertNotNull(actual);
         Assert.assertEquals(actual.get(0), testList.get(0));
         Assert.assertEquals(actual.get(1), testList.get(1));
         Assert.assertEquals(actual.get(2), testList.get(2));
