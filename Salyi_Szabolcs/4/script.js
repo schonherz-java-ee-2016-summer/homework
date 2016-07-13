10f2f0edb7252405a4926521385c0e98d495464a
SZSalyi <salyi.szabi@gmail.com>
2016-07-13 21:50:37 +0200

    Homework_4 - Last corrections

--- Salyi_Szabolcs/4/script.js
+++ Salyi_Szabolcs/4/script.js
@@ -5,5 +5,10 @@ function colourGreen() {
 	document.getElementById("black").style.backgroundColor="#44FF44";
 }
 function colourRed() {
-	document.getElementById("black").style.backgroundColor="red";
+	document.getElementById("black").style.backgroundColor="#DD0000";
 } 
+function colourBigBox(c) {
+	var colour=document.getElementById(c);
+	colour=getComputedStyle(colour).backgroundColor;
+	document.getElementById("black").style.backgroundColor=colour;
+}
