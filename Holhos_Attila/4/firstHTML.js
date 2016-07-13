5c83c3cb3d23be0ea08ba2582a9ae691339694fa
AtiBaby <atibaby@citromail.hu>
2016-07-13 16:14:34 +0200

    Add Homework 4

---
+++ Holhos_Attila/4/firstHTML.js
@@ -0,0 +1,11 @@
+function blueColor() {
+    document.getElementById("bigsquare").style.backgroundColor = "rgb(93, 208, 255)";
+}
+
+function redColor() {
+    document.getElementById("bigsquare").style.backgroundColor = "red";
+}
+
+function greenColor() {
+    document.getElementById("bigsquare").style.backgroundColor = "green";
+}
