ed635df7352df5ecd497dcddefa9774201d020a0
Veress Armin <veress.armin@gmail.com>
2016-07-13 13:35:43 +0200

    Fourth homework.

---
+++ Veress_Armin/4/SquaresAction.js
@@ -0,0 +1,5 @@
+function katt1(){document.getElementById("Fourth").style.backgroundColor="rgb(216, 236, 243)";}
+
+function katt2(){document.getElementById("Fourth").style.backgroundColor="rgb(113, 218, 113)";}
+
+function katt3(){document.getElementById("Fourth").style.backgroundColor="rgb(255, 102, 51)";}
