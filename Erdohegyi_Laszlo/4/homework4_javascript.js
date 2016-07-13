525a07db4a51811626af9349a95aead2e3277d80
lac15 <laszlo.erdohegyi@gmail.com>
2016-07-13 16:53:40 +0200

    Added 4th homework

---
+++ Erdohegyi_Laszlo/4/homework4_javascript.js
@@ -0,0 +1,17 @@
+function changeBigToB() {
+    //var storedColor = document.getElementById("b").style.backgroundColor;
+	//document.getElementById("big").style.backgroundColor = storedColor;
+    
+    //var storedColor = $("#b").css(background-color);
+	//$("#big").css("background-color", storedColor);
+    
+    document.getElementById("big").style.backgroundColor = "lightblue";
+}
+
+function changeBigToG() {
+    document.getElementById("big").style.backgroundColor = "lightgreen";
+}
+
+function changeBigToR() {
+    document.getElementById("big").style.backgroundColor = "#f00000";
+}
