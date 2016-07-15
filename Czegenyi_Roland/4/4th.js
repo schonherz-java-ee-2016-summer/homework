f8b04a8421c7522484b55f205aa0442461d9fef6
Czégényi Roland <czegenyi.roland@gmail.com>
2016-07-15 23:02:18 +0200

    Changing doctype, removing XHTML specification,and add class selector to jQuery on() method.

--- Czegenyi_Roland/4/4th.js
+++ Czegenyi_Roland/4/4th.js
@@ -1,7 +1,7 @@
 /* JavaScript Document */
-
-$(document).on('click', 'div', function () {
- 		"use strict";
- 		var color = $(this).css('background-color');
-		$('#square2').css("background-color", color);
+$(document).on('click', 'div.square', function () {
+		"use strict";
+        var color = $(this).css('background-color');
+        $('#square2').css("background-color", color);
 });
+
