f66ced314c7bf956b7b7c7056635e0df63f7cdce
Czégényi Roland <czegenyi.roland@gmail.com>
2016-07-15 23:05:06 +0200

    Fixing indentation.

--- Czegenyi_Roland/4/4th.js
+++ Czegenyi_Roland/4/4th.js
@@ -1,6 +1,6 @@
 /* JavaScript Document */
 $(document).on('click', 'div.square', function () {
-		"use strict";
+	    "use strict";
         var color = $(this).css('background-color');
         $('#square2').css("background-color", color);
 });
