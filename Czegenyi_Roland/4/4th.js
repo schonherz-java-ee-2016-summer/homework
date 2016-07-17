70e2d0daad2bee5d0aadaab4a56c06710ca6ac49
Czégényi Roland <czegenyi.roland@gmail.com>
2016-07-17 18:04:59 +0200

    Updating js. Alternative solution.

--- Czegenyi_Roland/4/4th.js
+++ Czegenyi_Roland/4/4th.js
@@ -1,7 +1,18 @@
-/* JavaScript Document */
+// JavaScript Document
+
 $(document).on('click', 'div.square', function () {
-	    "use strict";
-        var color = $(this).css('background-color');
-        $('#square2').css("background-color", color);
+    "use strict";
+    var color = $(this).css('background-color');
+    $('#square2').css("background-color", color);
 });
 
+//Alternative solution.
+/*
+$(document).ready(function () {
+    "use strict";
+    $(".square").click(function () {
+        var color = $(this).css('background-color');
+        $('#square2').css("background-color", color);
+    });
+});
+ */
