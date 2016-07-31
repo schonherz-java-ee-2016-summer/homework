63b96aa75a39e944f34639d095e32794557a539d
Bódi Balázs <bmbali1992@gmail.com>
2016-07-31 21:14:11 +0200

    7th homework : some fix added

--- Bodi_Balazs/7/BasicBloggerWebapp/src/main/java/hu/schonherz/basicblogger/rest/CommentRestController.java
+++ Bodi_Balazs/7/BasicBloggerWebapp/src/main/java/hu/schonherz/basicblogger/rest/CommentRestController.java
@@ -1,8 +1,6 @@
 package hu.schonherz.basicblogger.rest;
 
-import hu.schonherz.basicblogger.jdbcTemplates.BlogJDBCTemplate;
 import hu.schonherz.basicblogger.jdbcTemplates.CommentJDBCTemplate;
-import hu.schonherz.basicblogger.pojo.Blog;
 import hu.schonherz.basicblogger.pojo.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,36 +13,20 @@ import java.util.List;
 /**
  * Created by bmbal on 2016. 07. 31..
  */
-//@RestController
-//@RequestMapping(path = "/rest", produces = "application/json")
-public class CoreRestController {
+@RestController
+public class CommentRestController {
 
     @Autowired
     private CommentJDBCTemplate commentJDBCTemplate;
 
-    @Autowired
-    private BlogJDBCTemplate blogJDBCTemplate;
-
-    @RequestMapping(path = "/createblog", method = RequestMethod.POST)
-    public ResponseEntity addPost(@RequestBody Blog blog) {
-        blogJDBCTemplate.createBlog(blog);
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
     @RequestMapping(path = "/createcomment/{blog_id}", method = RequestMethod.POST)
-    public ResponseEntity addComment(@RequestBody Comment comment, @PathVariable("blog_id") int blog_id){
+    public ResponseEntity createComment(@RequestBody Comment comment, @PathVariable("blog_id") int blog_id){
         commentJDBCTemplate.createComment(comment);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/blogs", method = RequestMethod.GET)
-    public ResponseEntity listPosts(Model model) {
-        List<Blog> blogs = blogJDBCTemplate.getAllBlog();
-        return new ResponseEntity(blogs, HttpStatus.OK);
-    }
-
     @RequestMapping(path = "/blog/{blog_id}", method = RequestMethod.GET)
-    public ResponseEntity getCommets(@PathVariable("blog_id") int blog_id, Model model){
+    public ResponseEntity getComments(@PathVariable("blog_id") int blog_id, Model model){
         List<Comment> comments = commentJDBCTemplate.getAllComments(blog_id);
         return new ResponseEntity(comments, HttpStatus.OK);
     }
