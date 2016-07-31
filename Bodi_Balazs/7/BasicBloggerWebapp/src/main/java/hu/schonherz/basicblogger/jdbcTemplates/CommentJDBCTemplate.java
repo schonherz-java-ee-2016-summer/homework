d05b2103a777984eff45a5b3ec1eacc1adc1b947
Bódi Balázs <bmbali1992@gmail.com>
2016-07-31 21:03:33 +0200

    7th homework finished

---
+++ Bodi_Balazs/7/BasicBloggerWebapp/src/main/java/hu/schonherz/basicblogger/jdbcTemplates/CommentJDBCTemplate.java
@@ -0,0 +1,34 @@
+package hu.schonherz.basicblogger.jdbcTemplates;
+
+import hu.schonherz.basicblogger.dao.CommentDao;
+import hu.schonherz.basicblogger.mappers.CommentMapper;
+import hu.schonherz.basicblogger.pojo.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by bmbal on 2016. 07. 31..
+ */
+
+@Repository
+public class CommentJDBCTemplate implements CommentDao{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Comment> getAllComments(int blog_id) {
+        String sql = "SELECT id, blog_id, author, content FROM public.comment WHERE blog_id = ?";
+        List<Comment> comments = jdbcTemplate.query(sql, new CommentMapper(), blog_id);
+        return comments;
+    }
+
+    @Override
+    public void createComment(Comment comment) {
+        String sql = "Insert into public.comment (blog_id, author, content) VALUES (?,?,?);";
+        jdbcTemplate.update(sql, comment.getBlog_id(), comment.getAuthor(), comment.getContent());
+    }
+}
